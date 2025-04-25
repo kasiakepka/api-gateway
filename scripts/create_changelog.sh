584dd9914f002e140cbd556345f025c1b0162a24
kasiakepka <kasia.kepka23@gmail.com>
2025-04-25 09:24:03 +0200

    Update create_changelog.sh

--- scripts/create_changelog.sh
+++ scripts/create_changelog.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-LATEST_RELEASE=$2 # for testability
-
 # standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
 set -o errexit  # exit immediately when a command fails.
@@ -10,14 +8,35 @@ set -o pipefail # prevents errors in a pipeline from being masked
 
 RELEASE_TAG=$1
 
+# Set default repository if not provided
 REPOSITORY=${REPOSITORY:-kyma-project/api-gateway}
 GITHUB_URL=https://api.github.com/repos/${REPOSITORY}
 GITHUB_AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 CHANGELOG_FILE="CHANGELOG.md"
 
-if [ "${LATEST_RELEASE}"  == "" ]
-then
-  LATEST_RELEASE=$(curl -H "${GITHUB_AUTH_HEADER}" -sS "${GITHUB_URL}/releases/latest" | jq -r '.tag_name')
+# Extract major, minor, and patch version numbers
+set -- ${RELEASE_TAG//./ }
+MAJOR=$1
+MINOR=$2
+PATCH=$3
+
+# Fetch all release tags
+TAGS=$(curl -s -H "$GITHUB_AUTH_HEADER" "$GITHUB_URL/releases" | grep -o '"tag_name": "[^"]*"' | sed -E 's/"tag_name": "([^"]*)"/\1/' | sort -V)
+
+# Determine the previous release based on versioning rules
+LATEST_TAG=""
+if [ $PATCH -ne 0 ]; then
+  LATEST_TAG=$(echo "$TAGS" | grep -E "^${MAJOR}\.${MINOR}\." | tail -1)
+elif [ "$MINOR" -ne 0 ]; then
+  PREV_MINOR=$(($MINOR - 1))
+  LATEST_TAG=$(echo "$TAGS" | grep -E "^${MAJOR}\.${PREV_MINOR}\." | head -n 1)
+else
+  PREV_MAJOR=$(($MAJOR - 1))
+  LATEST_TAG=$(echo "$TAGS" | grep -E "^${PREV_MAJOR}\." | head -n 1)
 fi
 
-echo -e "\n**Full changelog:** https://github.com/$REPOSITORY/compare/${LATEST_RELEASE}...${RELEASE_TAG}" >> ${CHANGELOG_FILE}
+# Fetch commit history between the previous and current release
+echo -e "\n**Full changelog:** https://github.com/$REPOSITORY/compare/${LATEST_TAG}...${RELEASE_TAG}" >> ${CHANGELOG_FILE}
+
+
+gh issue list --repo "github.tools.sap/kyma/security-scans-modular" --assignee "I304607" --state open | awk '{print "https://github.tools.sap/kyma/security-scans-modular/issues/" $1, $4}' | sort -t: -k 3
